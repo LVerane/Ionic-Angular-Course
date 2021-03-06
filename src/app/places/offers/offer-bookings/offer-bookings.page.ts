704ef4f9309af947a38bebcc5e4673b0caee02cc
Lucio Verane <verane64@gmail.com>
2021-03-05 18:26:13 -0600

    content added to offers and offer-booking pages, forward and backward navigation added to pages with content

--- src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-offer-bookings',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offer-bookings.page.scss'],
 })
 export class OfferBookingsPage implements OnInit {
+  constructor(private navCtrl: NavController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onOfferBooking() {
+    this.navCtrl.navigateBack('/places/tabs/offers');
   }
-
 }
