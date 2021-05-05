47ce30d77963a200d30a1249760852ca26d85085
Lucio Verane <verane64@gmail.com>
2021-05-05 14:26:12 -0500

    user signup and login added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { key } from '../../environments/environment-api';
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private _userIsAuthenticated = false;
-  private _userId = 'abc';
+  private _userId = null;
 
   get userIsAuthenticated() {
     return this._userIsAuthenticated;
@@ -15,10 +26,20 @@ export class AuthService {
     return this._userId;
   }
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  login() {
-    this._userIsAuthenticated = true;
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key.firebase_key}`,
+      { email: email, password: password, returnSecureToken: true }
+    );
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${key.firebase_key}`,
+      { email: email, password: password, returnSecureToken: true }
+    );
   }
 
   logout() {
